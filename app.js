b25500eac0f03311af214fe8d0bb008f6aa6c470
James Ross <git@james-ross.co.uk>
2020-04-02 16:44:12 +0100

    feat: Basic page structure and styling (orange)

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var usersRouter = require("./routes/users");
 
 var app = express();
 
+app.locals.applicationTitle = "Application Deployer";
+app.locals.copyrightYear = new Date().getFullYear();
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
